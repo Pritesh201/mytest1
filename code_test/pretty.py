1cce66183cf90548c3420707b074718738950c80
Pritesh <priteshsingh@Priteshs-MacBook-Air.local>
2024-06-09 15:31:27 +0530

    adding new branch

---
+++ code_test/pretty.py
@@ -0,0 +1,14 @@
+print("Hello from Pretty module.!!!")
+
+alpha = 63
+
+def greeter(name):
+    return f"Good day, {name}"
+
+
+print("Name of the module:", __name__)
+
+
+if __name__ == '__main__':
+    #some secret code here
+    print("secret code here")
